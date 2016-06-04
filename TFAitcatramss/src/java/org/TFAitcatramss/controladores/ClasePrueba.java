a1646368973ac950e72a36703511a43cedc46da6
jonathan <00500411@uca.edu.sv>
2016-06-03 18:26:04 -0600

    nuevo commit aunque no se que es eso

---
+++ TFAitcatramss/src/java/org/TFAitcatramss/controladores/ClasePrueba.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.TFAitcatramss.controladores;
+
+/**
+ *
+ * @author astreo
+ */
+public class ClasePrueba {
+    //va ivan esto agrege yo 
+    //soy un comentario
+}
